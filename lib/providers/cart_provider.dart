4a4c9dd72ede849bb1c604ca4101719358734ba4
prime-infinity <soe.a.prime@gmail.com>
2024-10-27 07:00:38 -0700

    used generated provider notifier

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,17 +1,14 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/models/product.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
   //initial value
   @override
   Set<Product> build() {
-    return const {
-      Product(
-          id: '4',
-          title: 'Red Backpack',
-          price: 14,
-          image: 'assets/products/backpack.png'),
-    };
+    return const {};
   }
 
   //method to update the state
@@ -30,6 +27,6 @@ class CartNotifier extends Notifier<Set<Product>> {
 }
 
 //pretty verbose code, just try to understand
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+/*final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
   return CartNotifier();
-});
+});*/
